5e545009bad99eaa027d0139de42ab9b5cbdc0c6
Max <langweil.m@northeastern.edu>
2022-11-06 15:17:44 -0500

    added skeleton code

--- Othello.py
+++ Othello.py
@@ -1,3 +1,36 @@
-
+import turtle
 class OthelloPiece():
-    pass
+    def __init__(self, color):
+        self.color = color
+    def create_game_piece(self, color):
+        """ 
+        Creates black or white game piece
+
+        Input: Self, String
+        Output: 
+        """
+        pass
+
+class Game_Board():
+    def __init__(self, color, rows):
+        self.color = color
+        self.rows = rows
+    def draw_game_board(self):
+        pass   
+
+class Othello():
+    def __init__(self):
+        pass
+    def place_piece(self):
+        pass
+
+    def show_score(self):
+        pass
+    def next_turn(self):
+        pass
+    def determine_first_player(self):
+        pass
+    def flip_pieces(self):
+        pass
+    def winner(self):
+        pass

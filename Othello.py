a115e67a1700776c9ffbdd689744f1f70284a599
Max <langweil.m@northeastern.edu>
2022-11-12 15:45:19 -0500

    rendered the screen for the game

--- Othello.py
+++ Othello.py
@@ -1,4 +1,11 @@
-import turtle
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
+SCREEN_TITLE = "Othello"
+SCALING = 2.0
+
+
 class OthelloPiece():
     def __init__(self, color):
         self.color = color
@@ -9,18 +16,26 @@ class OthelloPiece():
         Input: Self, String
         Output: 
         """
-        pass
+
+        
+
 
 class Game_Board():
     def __init__(self, color, rows):
         self.color = color
         self.rows = rows
     def draw_game_board(self):
-        pass   
+        arcade.draw_rectangle_filled(400, 400, 400, 400, arcade.color.FOREST_GREEN)
+   
 
-class Othello():
+class Othello(arcade.Window):
+
     def __init__(self):
-        pass
+        """Initialize the game
+        """
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.WHITE)
+
     def place_piece(self):
         pass
 
@@ -34,3 +49,14 @@ class Othello():
         pass
     def winner(self):
         pass
+    def start(self):
+        pass
+    def on_draw(self):
+        """ Called whenever we need to draw the window. """
+        arcade.start_render()
+        arcade.draw_rectangle_filled(400, 400, 400, 400, arcade.color.FOREST_GREEN)
+
+# Main code entry point
+if __name__ == "__main__":
+    game = Othello()
+    arcade.run()
